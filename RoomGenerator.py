36fa4bafb919cd106a1b20124be13b56dde49b7d
idk-MoraSama <aryao2.2000@gmail.com>
2026-04-11 14:57:55 +0530

    minor fixing added propagation and entropy

--- RoomGenerator.py
+++ RoomGenerator.py
@@ -10,7 +10,7 @@ colors = {
 pygame.init()
 
 main_screen = pygame.display.set_mode([1280,720])
-render_surf = pygame.Surface([320,180])
+render_surf = pygame.Surface([640,360])
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,image,size):
@@ -43,8 +43,30 @@ class Room():
                 self.image_set.append(self.image.subsurface((x,y),(self.tilesize,self.tilesize)))
         
         self.generate_grid()
-        for cell in self.grid:
-            self.collapse_cell(cell)
+        while sum([not cell["collapsed"] for cell in self.grid.values()]):
+            self.collapse_cell(self.get_lowest_entropy_cell())
+
+    def get_lowest_entropy_cell(self):
+        cells = []
+        min_entropy = 100
+        for x,y in self.grid:
+
+            if self.grid[(x,y)]["collapsed"]:
+                continue
+
+            corners = [(x,y),(x+1,y),(x,y+1),(x+1,y+1)]
+
+            entropy = 0
+            for corner in corners:
+                entropy += len(self.corners[corner])
+            
+            if entropy < min_entropy:
+                min_entropy = entropy
+                cells.clear()
+                cells.append((x,y))
+            elif entropy == min_entropy:
+                cells.append((x,y))
+        return random.choice(cells)
 
     def generate_grid(self):
         for y in range(self.size[1]+1):
@@ -57,6 +79,25 @@ class Room():
                     "collapsed" : False,
                     "tile" : None 
                 }
+
+    def collapse_corners(self,corners):
+        min_entropy = 100
+        candidates = []
+        while corners:
+            for corner in corners:
+                entropy = len(self.corners[corner])
+                if min_entropy > entropy:
+                    min_entropy = entropy
+                    candidates.clear()
+                    candidates.append(corner)
+                elif min_entropy == entropy:
+                    candidates.append(corner)
+            removed = random.choice(candidates)
+            min_entropy = 100
+            candidates = []
+            corners.remove(removed)
+            self.corners[removed] = [random.choice(self.corners[removed])]
+            self.propagate_corner(removed)
 
     def collapse_cell(self,pos:tuple):
         if self.grid[pos]["collapsed"]:
@@ -71,6 +112,7 @@ class Room():
                 corner.pop(random.randint(0,len(corner)-1))
             
             self.corners[cord] = corner
+            self.propagate_corner(cord)
         
         corners = [self.corners[cord][0] for cord in corner_cords]
         candidates = []
@@ -83,6 +125,22 @@ class Room():
         self.grid[pos]["tile"] = Tile(pos,self.image_set[random.choice(candidates)],32)
         self.grid[pos]["collapsed"] = True
 
+    def propagate_corner(self,corner):
+        neighbours = []
+        current_corner = self.corners[corner]        
+        if current_corner != [None]:
+            current_corner = current_corner*5
+
+        for y in range(corner[1]-1,corner[1]+1):
+            for x in range(corner[0]-1,corner[0]+1):
+                if (x,y) in self.corners:
+                    if len(self.corners[(x,y)]) == 1:
+                        continue
+                    neighbours.append((x,y))
+                
+        for cord in neighbours:
+            self.corners[cord].extend(current_corner)
+
 
 if __name__ == "__main__":
 
@@ -90,20 +148,26 @@ if __name__ == "__main__":
         pygame.quit()
         exit()
     
-    room1 = Room("grass-tiles.png","sockets.yaml",32,(10,6))
-
+    room1 = Room("grass-tiles.png","sockets.yaml",32,(20,15))
+    grid = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Exit_Game()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                tilegroup.empty()
+                room1 = Room("grass-tiles.png","sockets.yaml",32,(20,15))
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_g:
+                grid = not grid
         
         render_surf.fill(colors["BG"])
         tilegroup.draw(render_surf)
 
-        for x in range(0,render_surf.width,32):
-            pygame.draw.line(render_surf,(0,0,0),(x,0),(x,400),1)
-        for y in range(0,render_surf.height,32):
-            pygame.draw.line(render_surf,(0,0,0),(0,y),(400,y),1)
+        if grid:
+            for x in range(0,render_surf.width,32):
+                pygame.draw.line(render_surf,(0,0,0),(x,0),(x,720),1)
+            for y in range(0,render_surf.height,32):
+                pygame.draw.line(render_surf,(0,0,0),(0,y),(1280,y),1)
         
         main_screen.blit(pygame.transform.scale_by(render_surf,main_screen.height/render_surf.height),(0,0))
         pygame.display.update()
--- roughPad.py
+++ roughPad.py
@@ -1,25 +1,27 @@
-import pygame
-import sys
+# import pygame
+# import sys
 
-pygame.init()
+# pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+# screen = pygame.display.set_mode((400,400))
 
-img = pygame.image.load("grass-tiles.png").convert_alpha()
-a = 300
+# img = pygame.image.load("grass-tiles.png").convert_alpha()
+# a = 300
 
-clock = pygame.time.Clock()
+# clock = pygame.time.Clock()
 
-while True:
-    delta = clock.tick(60)
-    a -= 1
+# while True:
+#     delta = clock.tick(60)
+#     a -= 1
 
-    if a <= 0:
-        pygame.quit()
-        sys.exit()
+#     if a <= 0:
+#         pygame.quit()
+#         sys.exit()
 
-    for y in range(0,img.height - 31,32):
-        for x in range(0,img.width- 31,32):
-            screen.blit(img.subsurface((x,y),(32,32)),(x*1.2,y*1.2))
+#     for y in range(0,img.height - 31,32):
+#         for x in range(0,img.width- 31,32):
+#             screen.blit(img.subsurface((x,y),(32,32)),(x*1.2,y*1.2))
     
-    pygame.display.update()
+#     pygame.display.update()
+
+print(len([None,1,None]))
